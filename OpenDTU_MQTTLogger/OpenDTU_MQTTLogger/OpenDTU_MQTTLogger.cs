bdf0a294c6736eff79a6c1387b3160d94aee5387
Victor van Acht <victor@octaal.nl>
2023-07-08 10:03:02 +0200

    Initial Visual Studio C# project

---
+++ OpenDTU_MQTTLogger/OpenDTU_MQTTLogger/OpenDTU_MQTTLogger.cs
@@ -0,0 +1,10 @@
+namespace OpenDTU_MQTTLogger
+{
+    public partial class OpenDTU_MQTTLogger : Form
+    {
+        public OpenDTU_MQTTLogger()
+        {
+            InitializeComponent();
+        }
+    }
+}
